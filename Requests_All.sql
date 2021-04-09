c4c4bf94a6cc00d737bc12508b972389344030e4
Vandana Shah <vp25@cornell.edu>
2021-04-09 11:38:41 -0400

    Update Requests_All.sql

--- Requests_All.sql
+++ Requests_All.sql
@@ -1,17 +1,24 @@
+WITH parameters AS (
+    SELECT
+        /* Choose a start and end date for the requests period */
+        '2000-01-01'::date AS start_date,
+        '2022-01-01'::date AS end_date
+        )
 SELECT
+    (SELECT start_date::varchar FROM parameters) || ' to ' || (SELECT end_date::varchar FROM parameters) AS date_range,
     cr.id,
     cr.request_date,
     cr.request_type,
     cr.requester_id,
     cr.status,
     cr.pickup_service_point_id,
-    spe.service_point_discovery_display_name as pickup_service_point_name,
-    spe.location_discovery_display_name as pickup_location_name,
+ --   spe.service_point_discovery_display_name as pickup_service_point_name,
+ --   spe.location_discovery_display_name as pickup_location_name,
     spe.library_name as pickup_library_name,
     cr.item_id,
     cr.fulfilment_preference,
     ie.item_id,
-    ie.call_number,
+    he.call_number,
     ie.barcode,
     ie.material_type_name,
     ie.holdings_record_id,
@@ -37,4 +44,16 @@ LEFT JOIN public.inventory_service_points as isp
 	ON cr.pickup_service_point_id = isp.id	
 left join local.service_points_ext as spe
 	on cr.pickup_service_point_id =spe.service_point_id
-	;
+where 
+request_date::DATE >= (
+            SELECT
+                start_date
+           FROM
+                parameters)
+    	AND request_date::DATE < (
+               SELECT
+                 end_date
+                    FROM
+                        parameters)
+                       ;
+	
