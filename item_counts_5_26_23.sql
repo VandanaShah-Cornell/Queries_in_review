5a59487ed71f36fcea4cdbef1c7d1178b0d7212c
Vandana Shah <vp25@cornell.edu>
2023-05-26 12:20:44 -0400

    Update and rename item_counts_5_23_23.sql to item_counts_5_26_23.sql

--- item_counts_5_26_23.sql
+++ item_counts_5_26_23.sql
@@ -12,7 +12,7 @@ marc_formats AS
     	 ),
 
  
---bring in holdings details in order to exclude some locations, and to identify micro-materials 
+--bring in holdings details in order to exclude some locations
     	 
 holdings AS
     (SELECT 
@@ -20,14 +20,7 @@ holdings AS
    	he.instance_id,
    	he.permanent_location_id,
    	he.permanent_location_name AS holdings_permanent_location_name
-   
- /*  	--identifying micro items via call number and title
-   	CASE 
-	  WHEN ((concat_ws (' ',he.call_number_prefix,he.call_number,he.call_number_suffix) iLIKE '%film%'OR ii.title ilike '%[film%]%'))  THEN 'film'
-  	WHEN ((concat_ws (' ',he.call_number_prefix,he.call_number,he.call_number_suffix) ILIKE '%fiche%' OR ii.title ilike '%[fiche%]%'))  THEN 'fiche' 
-   	WHEN ((concat_ws (' ',he.call_number_prefix,he.call_number,he.call_number_suffix) iLIKE '%micro%'OR ii.title ilike '%[micro%]%'))   THEN 'micro' 
-   	ELSE 'Not micro' END AS micro_from_callNum_title*/
-   	
+ 
 	   	   	   	
    	FROM folio_reporting.holdings_ext AS he
   
@@ -40,7 +33,7 @@ holdings AS
 'Biochem Reading Room', 'Borrow Direct', 'CISER', 'cons,opt', 'Engineering', 'Engineering Reference', 'Engr,wpe',
 'Entomology', 'Food Science', 'Law Technical Services', 'LTS Review Shelves', 'LTS E-Resources & Serials','Mann Gateway',
 'Mann Hortorium', 'Mann Hortorium Reference', 'Mann Technical Services', 'Iron Mountain', 'Interlibrary Loan%', 'Phys Sci',
-'RMC Technical Services', 'No Library','x-test', 'z-test location' ])) --OR he.permanent_location_name IS NULL)
+'RMC Technical Services', 'No Library','x-test', 'z-test location' ])) 
 
 AND he.call_number NOT ILIKE ALL(ARRAY['on order%', 'in process%', 'Available for the library to purchase', 
  'On selector%', '%film%','%fiche%', '%micro%', '%vault%']) 
@@ -53,7 +46,7 @@ ORDER BY he.instance_id, he.holdings_id
 ),
 
 
---bring in item details and join to holdings subquery so as to exclude all instance ids that are not in the holdings subquery
+--bring in item details 
 items AS
 (SELECT 
 DISTINCT ie.item_id, 
@@ -81,7 +74,7 @@ WHERE concat_ws (' ',ie.effective_call_number_prefix,ie.effective_call_number,ie
 --combine holdings and instances subqueries and include format descriptors from the translation table 'local_core.vs_folio_physical_material_formats'
 combined AS   
 (SELECT DISTINCT
-   	hh.instance_id,
+  	hh.instance_id,
    	hh.holdings_id,
     hh.holdings_permanent_location_name,
    	ite.item_id,
@@ -99,20 +92,20 @@ combined AS
     fmg.folio_format_type_acrl_nces_groups
     
    	
-  	FROM holdings AS hh
-  	LEFT JOIN marc_formats AS fm ON hh.instance_id ::uuid = fm.instance_id
+  	FROM marc_formats AS fm
+  	LEFT JOIN holdings AS hh ON hh.instance_id ::uuid = fm.instance_id
   	LEFT JOIN items AS ite ON ite.holdings_id=hh.holdings_id
-	  LEFT JOIN folio_reporting.locations_libraries AS ll ON hh.permanent_location_id = ll.location_id  
-	  LEFT JOIN folio_reporting.locations_libraries AS lz ON ite.permanent_location_id = lz.location_id  
+	LEFT JOIN folio_reporting.locations_libraries AS ll ON hh.permanent_location_id = ll.location_id  
+	LEFT JOIN folio_reporting.locations_libraries AS lz ON ite.permanent_location_id = lz.location_id  
    	LEFT JOIN local_core.vs_folio_physical_material_formats AS fmg ON fmg.leader0607 = fm.leader0607
     LEFT JOIN local_core.lm_adc_loc_translation AS adc2 ON 	hh.holdings_permanent_location_name=adc2.permanent_location_name)
     
     
    SELECT 
     count(DISTINCT cc.instance_id) AS countDistinct_instanceID,
-   	count(cc.instance_id) AS count_instanceID,
-   	count(cc.holdings_id) AS count_holdings_ID,
- 	  count(cc.item_id) AS count_itemID,
+ 	count(cc.instance_id) AS count_instanceID,
+ 	count(DISTINCT cc.holdings_id) AS countDistinct_holdings_ID,
+ 	count(DISTINCT cc.item_id) AS counDistinct_itemID,
   	cc.item_material_type_name,
   	cc.record_created_fiscal_year,
    	cc.holdings_permanent_location_name,
@@ -126,13 +119,12 @@ combined AS
 	  cc.folio_format_type_adc_groups, 
 	  cc.folio_format_type_acrl_nces_groups
        FROM combined AS cc
-     WHERE cc.holdings_id IN (
-           SELECT holdings_id FROM holdings AS hh)
+    
    
     GROUP BY
            
     cc.item_material_type_name,
- 	  cc.record_created_fiscal_year,
+ 	cc.record_created_fiscal_year,
    	cc.holdings_permanent_location_name,
    	cc.item_permanent_location_name,
    	cc.holdings_adc_loc_translation,
@@ -141,5 +133,5 @@ combined AS
   	cc.leader0607,
   	cc.leader0607description,
   	cc.folio_format_type,
-	  cc.folio_format_type_adc_groups, 
-	  cc.folio_format_type_acrl_nces_groups
+	cc.folio_format_type_adc_groups, 
+	cc.folio_format_type_acrl_nces_groups
