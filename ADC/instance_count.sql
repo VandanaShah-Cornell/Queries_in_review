29cef2f45328e69f3bacb5967f2e459eaf0b3b74
Vandana Shah <vp25@cornell.edu>
2023-06-14 08:44:52 -0400

    Update instance_count.sql

--- ADC/instance_count.sql
+++ ADC/instance_count.sql
@@ -64,7 +64,7 @@ AND he.call_number NOT ILIKE ALL(ARRAY['on order%', 'in process%', 'Available fo
  'On selector%', '%film%','%fiche%', '%micro%', '%vault%']) 
 AND (he.discovery_suppress IS NOT TRUE OR he.discovery_suppress IS NULL OR he.discovery_suppress ='FALSE')
 AND (he.permanent_location_name IS NOT NULL)
-AND (ie.title NOT ILIKE '%microform%')
+AND (ie.title NOT ILIKE '%[microform]%')
 AND (ie.discovery_suppress IS NOT TRUE OR ie.discovery_suppress IS NULL OR ie.discovery_suppress = 'FALSE')
 --AND (mc.micro_by_007 NOT LIKE 'h')
 )
